7ec7dd4ba19ffc9741ebb52c8bfa6ff6244dadc5
Amit Bansal <amitbansal26@gmail.com>
2016-08-05 08:18:45 +0530

    updated

--- src/main/java/com/example/controller/CFMetricsController.java
+++ src/main/java/com/example/controller/CFMetricsController.java
@@ -3,7 +3,8 @@ package com.example.controller;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.cloudfoundry.client.lib.CloudCredentials;
@@ -11,8 +12,6 @@ import org.cloudfoundry.client.lib.CloudFoundryClient;
 import org.cloudfoundry.client.lib.domain.ApplicationStats;
 import org.cloudfoundry.client.lib.domain.CloudApplication;
 import org.cloudfoundry.client.lib.domain.CloudService;
-import org.cloudfoundry.client.lib.domain.CloudSpace;
-import org.cloudfoundry.client.lib.domain.InstancesInfo;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,46 +22,29 @@ public class CFMetricsController {
 
 	
 	@RequestMapping(value="/getmetrics", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public static ApplicationStats getMetrics(){
+	public static List<ApplicationStats> getMetrics(){
 		
 	 CloudCredentials credentials = new CloudCredentials("amit.bansal@capgemini.com", "trtr22");
 	        CloudFoundryClient client = new CloudFoundryClient(credentials, getTargetURL("https://api.run.pivotal.io"));
 	        client.login();
-	        Map<String, Integer> map = client.getApplications().get(0).getResources();
-	        
-	        System.out.println(map.toString());
-	        ApplicationStats applicationStats = client.getApplicationStats("spring-music");
-	        applicationStats.getRecords().get(0);
-	        
-	        System.out.println("Disk:: " + applicationStats.getRecords().get(0).getUsage().getDisk());
-	        System.out.println("Spaces:");
-	        for (CloudSpace space : client.getSpaces()) {
-	            System.out.printf("  %s\t(%s)%n", space.getName(), space.getOrganization().getName());
-	        }
-	        
-	        System.out.printf("%nApplications:%n");
+	        List<ApplicationStats> applicationDataList = new ArrayList<>();
 	        for (CloudApplication application : client.getApplications()) {
-	            System.out.printf("  %s%n", application.getName());
-	        }
-
-	        System.out.printf("%nServices%n");
-	        for (CloudService service : client.getServices()) {
-	            System.out.printf("  %s\t(%s)%n", service.getName(), service.getLabel());
-	        }
+	        	applicationDataList.add(client.getApplicationStats(application.getName()));
 	        
-	        return applicationStats;
+	        }
+	        return applicationDataList;
 
 	}
 	
 	
 	@RequestMapping(value="/getapps", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public static InstancesInfo getApplication(){
+	public static List<CloudApplication> getApplication(){
 		
 	 CloudCredentials credentials = new CloudCredentials("amit.bansal@capgemini.com", "trtr22");
 	        CloudFoundryClient client = new CloudFoundryClient(credentials, getTargetURL("https://api.run.pivotal.io"));
 	        client.login();
 	        
-	        return client.getApplicationInstances("spring-music");
+	        return client.getApplications();
 
 	}
 
