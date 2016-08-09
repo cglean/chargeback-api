8802bd524321da4817ad4f968a49f16bcae912ed
Amit Bansal <Amit.X.Bansal.-ND@disney.com>
2016-08-09 14:00:13 +0530

    Changed For Unuitilised Memory

--- src/main/java/com/example/controller/CFMetricsController.java
+++ src/main/java/com/example/controller/CFMetricsController.java
@@ -5,13 +5,11 @@ import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.cloudfoundry.client.lib.CloudCredentials;
 import org.cloudfoundry.client.lib.CloudFoundryClient;
 import org.cloudfoundry.client.lib.domain.ApplicationStats;
 import org.cloudfoundry.client.lib.domain.CloudApplication;
-import org.cloudfoundry.client.lib.domain.CloudService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,39 +18,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CFMetricsController {
 
-	
-	@RequestMapping(value="/getmetrics", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public static List<ApplicationStats> getMetrics(){
-		
-	 CloudCredentials credentials = new CloudCredentials("amit.bansal@capgemini.com", "trtr22");
-	        CloudFoundryClient client = new CloudFoundryClient(credentials, getTargetURL("https://api.run.pivotal.io"));
-	        client.login();
-	        List<ApplicationStats> applicationDataList = new ArrayList<>();
-	        for (CloudApplication application : client.getApplications()) {
-	        	applicationDataList.add(client.getApplicationStats(application.getName()));
-	        
-	        }
-	        return applicationDataList;
+	@RequestMapping(value = "/getmetrics", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<ApplicationStats> getMetrics() {
 
-	}
-	
-	
-	@RequestMapping(value="/getapps", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public static List<CloudApplication> getApplication(){
-		
-	 CloudCredentials credentials = new CloudCredentials("amit.bansal@capgemini.com", "trtr22");
-	        CloudFoundryClient client = new CloudFoundryClient(credentials, getTargetURL("https://api.run.pivotal.io"));
-	        client.login();
-	        
-	        return client.getApplications();
+		CloudFoundryClient client = loginCloudFoundry();
+		List<ApplicationStats> applicationDataList = new ArrayList<>();
+		for (CloudApplication application : client.getApplications()) {
+			applicationDataList.add(client.getApplicationStats(application.getName()));
+
+		}
+		return applicationDataList;
 
 	}
 
-	private static URL getTargetURL(String target) {
-	        try {
-	            return URI.create(target).toURL();
-	        } catch (MalformedURLException e) {
-	            throw new RuntimeException("The target URL is not valid: " + e.getMessage());
-	        }
-	    }
+	@RequestMapping(value = "/getFreeMematOrg", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public long getFreeMematOrg() {
+
+		CloudFoundryClient client = loginCloudFoundry();
+		long memoryquota = client.getQuotaByName("trial", true).getMemoryLimit() * 1024 * 1024;
+		System.out.println("Memory Quota ::::" + memoryquota);
+		for (CloudApplication application : client.getApplications()) {
+			memoryquota = memoryquota
+					- ((client.getApplicationStats(application.getName()).getRecords().get(0).getUsage().getMem()));
+
+		}
+		return memoryquota;
+	}
+
+	private URL getTargetURL(String target) {
+		try {
+			return URI.create(target).toURL();
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("The target URL is not valid: " + e.getMessage());
+		}
+	}
+
+	private CloudFoundryClient loginCloudFoundry() {
+		CloudCredentials credentials = new CloudCredentials("amit.bansal@capgemini.com", "trtr22");
+		CloudFoundryClient client = new CloudFoundryClient(credentials, getTargetURL("https://api.run.pivotal.io"));
+		client.login();
+		return client;
+	}
 }
